d857a114fc164549ef76f7d11f7213155089db94
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-20 23:25:16 -0700

    recvice details

--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { globalStyles } from '../styles/global'
 
-
 export default function ReviewDetails() {
     return (
         <View style={globalStyles.container}>
