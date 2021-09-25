959e7d3816c8a2ea69dcaa70fec42c4286da4386
gimnakatugampala <gimnakatugampala1@gmail.com>
2021-09-25 02:18:02 -0700

    navigating around

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,26 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Button } from 'react-native'
 import { globalStyles } from '../styles/global'
 
 
-export default function Home() {
+export default function Home({navigation}) {
+
+    
+
+    const presshandler = () =>{
+        // one screen to another
+        navigation.navigate('ReviewDetails')
+
+        // same
+        // navigation.push('ReviewDetails')
+
+
+    }
+
     return (
         <View style={globalStyles.container}>
             <Text style={globalStyles.titleText}>Home Screen</Text>
+            <Button title="go to review details" onPress={presshandler} />
         </View>
     )
 }
--- screens/ReviewDetails.js
+++ screens/ReviewDetails.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View,Button } from 'react-native'
 import { globalStyles } from '../styles/global'
 
-export default function ReviewDetails() {
+export default function ReviewDetails({navigation}) {
+
+    const presshandler = () => {
+        // Go Back
+        navigation.goBack()
+    }
+    
     return (
         <View style={globalStyles.container}>
             <Text>Review Details</Text>
+            <Button title="back to home screen" onPress={presshandler} />
         </View>
     )
 }
